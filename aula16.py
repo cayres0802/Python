eb503ea291642064c9be15ed23893ca3a9f03e41
cayres0802 <0123m.m4567@gmail.com>
2024-09-03 18:29:01 -0400

    commits

--- aula16.py
+++ aula16.py
@@ -4,7 +4,8 @@
 # verdadeiras.
 # Se qualquer valor for considerado falso,
 # a expressão inteira será avaliada naquele valor
-# São considerados falsy (que vc já viu)
+# São considerados falsy (que vc
+# já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
